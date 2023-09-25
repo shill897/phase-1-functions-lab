8bb9070298e50f12b4e7b54840595a98697f1ba6
mohamedshill <mohammedabdulahi248@gmail.com>
2023-09-25 11:21:24 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42); // Absolute difference between pickup location and 42nd Street
+}
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264; // Each block is 264 feet long
+}
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264;
+}
+function calculatesFarePrice(startBlock, endBlock) {
+    const distanceInFeet = distanceTravelledInFeet(startBlock, endBlock);
+
+    if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
